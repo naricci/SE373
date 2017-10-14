bee80daf1ece858546a827944a9c7ea3f70a007a
Nicholas Ricci <naricci@email.neit.edu>
2017-10-14 19:20:27 -0400

    added routes to about and form pages and built form page with pug

--- public_html/week2/lab2/routes/index.js
+++ public_html/week2/lab2/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET form page. */
+router.get('/form', function (req, res, next) {
+  res.render('form', { title: 'Form' });
+});
+
+/* POST form page. */
+router.post('/form', function (req, res, next) {
+  res.render('form', [{ name: 'res.body.name' }, {email: 'res.body.email'}, {comments: 'res.body.comments'}]);
+});
+
 module.exports = router;
