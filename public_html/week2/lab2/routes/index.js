6463502601e797698e0b321d7a0abff2d1ad7915
Nicholas Ricci <naricci@email.neit.edu>
2017-10-18 18:24:40 -0400

    fixed form page/results display

--- public_html/week2/lab2/routes/index.js
+++ public_html/week2/lab2/routes/index.js
@@ -1,34 +1,69 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-res.render('index', { title: 'Express' });
-});
+	res.render('index', { title: 'Express' })
+})
 
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-	res.render('about', { title: 'About' });
-});
+	res.render('about', { title: 'About' })
+})
 
 /* GET error page. */
 router.get('/errror', function (req, res, next) {
-	res.render('error', { title: 'Error'});
-});
+	res.render('error', { title: 'Error'})
+})
 
 /* GET form page. */
-router.get('/form', function (req, res, next) {
-	res.render('form', { title: 'Form' });
-});
+// router.get('/form', function (req, res, next) {
+// 	res.render('form', { title: 'Form' });
+// });
 
 /* POST form page. */
-router.post('/form', function (req, res, next) {
-	res.render('form', [{ name: 'res.body.name' }, {email: 'res.body.email'}, {comments: 'res.body.comments'}]);
-});
+// router.post('/form', function (req, res, next) {
+// 	res.render('form', [{ name: 'res.body.name' }, {email: 'res.body.email'}, {comments: 'res.body.comments'}]);
+// });
+
+/* GET & POST for form page. */
+router.get('/form', function (req, res, next) {
+
+	var msg = ''
+	if (req.query.empty === 'true') {
+		msg = 'Please enter a value'
+	}
+	res.render('form', {
+		title: 'Form Page',
+		message: msg
+	})
+}).post('/form', function (req, res, next) {
+	if (req.method === 'POST' && req.body.author.length) {
+
+		next()
+
+	} else {
+		res.render('form', {
+			title: 'Form Page',
+			message: 'Please enter a value'
+		})
+	}
+}).post('/form', function (req, res, next) {
+	if (req.method === 'POST' && req.body.author.length) {
+		res.render('results', {
+			title: 'Form Post Page',
+			author: req.body.author
+		})
+
+	} else {
+		res.redirect('/form?empty=true')
+
+	}
+})
 
 /* GET results page. */
 router.get('/results', function (req, res, next) {
-	res.render('results', { title: 'Results' });
-});
+	res.render('results', { title: 'Results' })
+})
 
-module.exports = router;
+module.exports = router
