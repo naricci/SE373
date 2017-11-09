65c1a4a8b88f877e2ba4a2cc98c5cca32c91e021
Nicholas Ricci <naricci@email.neit.edu>
2017-11-09 14:56:40 -0500

    ran eslint --fix on db.js file - lab4

--- public_html/week4/lab4/db.js
+++ public_html/week4/lab4/db.js
@@ -1,57 +1,57 @@
-var mongoose = require('mongoose');
-var debug = require('debug')('lab4:mongo');
-var gracefulShutdown;
-var dbURI = 'mongodb://localhost/Employees';
+var mongoose = require('mongoose')
+var debug = require('debug')('lab4:mongo')
+var gracefulShutdown
+var dbURI = 'mongodb://localhost/Employees'
 if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
+  dbURI = process.env.MONGOLAB_URI
 }
 
 mongoose.connect(dbURI, {
-    useMongoClient: true,
-    /* other options */
-});
+  useMongoClient: true,
+  /* other options */
+})
 
-mongoose.Promise = Promise;
+mongoose.Promise = Promise
 
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function () {
-    debug('Mongoose connected to ' + dbURI);
-});
+  debug('Mongoose connected to ' + dbURI)
+})
 mongoose.connection.on('error', function (err) {
-    debug('Mongoose connection error: ' + err);
-    process.exit(0);
-});
+  debug('Mongoose connection error: ' + err)
+  process.exit(0)
+})
 mongoose.connection.on('disconnected', function () {
-    debug('Mongoose disconnected');
-});
+  debug('Mongoose disconnected')
+})
 
 // CAPTURE APP TERMINATION / RESTART EVENTS
 // To be called when process is restarted or terminated
 gracefulShutdown = function (msg, callback) {
-    mongoose.connection.close(function () {
-        debug('Mongoose disconnected through ' + msg);
-        callback();
-    });
-};
+  mongoose.connection.close(function () {
+    debug('Mongoose disconnected through ' + msg)
+    callback()
+  })
+}
 // For nodemon restarts
 process.once('SIGUSR2', function () {
-    gracefulShutdown('nodemon restart', function () {
-        process.kill(process.pid, 'SIGUSR2');
-    });
-});
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2')
+  })
+})
 // For app termination
 process.on('SIGINT', function () {
-    gracefulShutdown('app termination', function () {
-        process.exit(0);
-    });
-});
+  gracefulShutdown('app termination', function () {
+    process.exit(0)
+  })
+})
 // For Heroku app termination
 process.on('SIGTERM', function () {
-    gracefulShutdown('Heroku app termination', function () {
-        process.exit(0);
-    });
-});
+  gracefulShutdown('Heroku app termination', function () {
+    process.exit(0)
+  })
+})
 
 process.on('exit', function (code) {
-    debug('About to exit with code: ', code);
-});
+  debug('About to exit with code: ', code)
+})
