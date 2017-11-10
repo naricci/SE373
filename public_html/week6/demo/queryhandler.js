05858e4cdaa77f192db78f83fa1cb2081941d810
Nicholas Ricci <naricci@email.neit.edu>
2017-11-09 19:00:02 -0500

    reworked search func from controller and added it into search module to be reusable

--- public_html/week6/demo/queryhandler.js
+++ public_html/week6/demo/queryhandler.js
@@ -1,36 +1,49 @@
 
-var debug = require('debug')('demo:queryHandler');
+var debug = require('debug')('demo:queryHandler')
 
 function search(query) {    
-    debug('search setup');
+	debug('search setup')
+
+	var where = {}
+
+	if (query) {
+
+		var column
+		for (column in query) {
+			if (column.indexOf('_') === -1) {
+				where[column] =  { $regex: new RegExp('.*?'+req.query[column].replace(/[\W_]+/g,'')+'.*') }
+			}
+	}
+
+	return where
 }
 
 function sort(query) {
-    debug('sort setup');
+	debug('sort setup')
 }
 
 module.exports.cors = function () {
-    return function(reg, res, next){
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');   
-        next();
-    };
-};
+	return function(reg, res, next){
+		res.header('Access-Control-Allow-Origin', '*')
+		res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
+		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')   
+		next()
+	}
+}
 
 module.exports.search = function () {
-  return function (req, res, next) {
-    // Add the search functionality to the request object
-    // req.where = search(req.query);
-    next();
-  };
-};
+	return function (req, res, next) {
+		// Add the search functionality to the request object
+		req.where = search(req.query)
+		next()
+	}
+}
 
 module.exports.sort = function () {
-  return function (req, res, next) {
-    // Add the options sort functionality to the request object
-    // if (!req.options) req.options = {};
-    // req.options.sort = sort(req.query);
-    next();
-  };
-};
+	return function (req, res, next) {
+		// Add the options sort functionality to the request object
+		// if (!req.options) req.options = {};
+		// req.options.sort = sort(req.query);
+		next()
+	}
+}
